5c1496e4cb57c63ec71909107d3b5f16a91d3c0a
Illin Tori <IllinTori@gmail.com>
2018-11-29 13:48:39 +0000

    Initial commit

---
+++ compiler.py
@@ -0,0 +1,14 @@
+file_to_open = "test.py"
+code = []
+with open(file_to_open, "r") as program:
+    for line in program:
+        code.append(line)
+
+key_word = {
+    "print":f_print
+}
+
+def f_print(arg)
+for line in code:
+    
+    
